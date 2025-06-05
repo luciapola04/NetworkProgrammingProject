4333b6659155e4422ac7dfd7860a0176f66894e5
Lucia Pola <lucia.pola@studio.unibo.it>
2025-06-05 13:19:01 +0200

    already cheanged

--- webServer.py
+++ webServer.py
@@ -3,7 +3,7 @@ import os
 import mimetypes
 from datetime import datetime
 
-HOST = 'localhost'  # Server IP address (localhost)
+HOST = 'localhost'  # localhost -> IP address : 127.0.0.1
 PORT = 8080         # Server port number
 WWW_ROOT = 'www'    # Directory where web files are stored
 
